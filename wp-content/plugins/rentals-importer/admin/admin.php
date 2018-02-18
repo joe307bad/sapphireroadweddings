43fb9beb55f1e2bd7ae1456a615c0f47d7d8cb0e
joebad <joe307bad@gmail.com>
2018-02-18 03:03:15 +0000

    prod updates

--- wp-content/plugins/rentals-importer/admin/admin.php
+++
@@ -1,4 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.core.min.js" ></script>
-
-<h1>Rentals Importer</h1>
-<button onclick="RentalsImporter.startImporter()">Start Importer</button>
